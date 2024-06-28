781f794b35c121d2d517d14ab7fb9119328febc7
Tanushree Thakur <tanushreethakur488@gmail.com>
2024-06-28 17:59:25 +0530

    initial change

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Tanushreethakur04/reactapp.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
